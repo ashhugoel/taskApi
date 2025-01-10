ec52bc6409d35a3aaa13d035404b35c7e45b1f0c
Ritik Goel <142885015+ashhugoel@users.noreply.github.com>
2025-01-10 21:19:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 
-mongoose.connect('mongodb+srv://temp:fVzlDtdZRNXoOb4E@cluster0.n545xqu.mongodb.net/userDB'); //THIS WOULD BE CLOSED AFTER USE 
+mongoose.connect('mongodb+srv://USERNAME:useUourOwnPass@cluster0.n545xqu.mongodb.net/userDB'); //THIS WOULD BE CLOSED AFTER USE 
 
 
 app.use('/api', authRoutes);
